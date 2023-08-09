360fbb0ee2f369518794d9b1f44b6351b2c1abc9
mingyang.gu <mingyang.gu@yeepay.com>
2023-08-09 10:35:03 +0800

    构建一个SpringBoot项目

--- src/test/java/com/yeepay/myspringbootproject/MySpringBootprojectApplicationTests.java
+++ src/test/java/com/yeepay/myspringbootproject/MySpringBootprojectApplicationTests.java
@@ -1,12 +1,10 @@
 package com.yeepay.myspringbootproject;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MySpringBootprojectApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
